b4622fc1798d60a2a6018359f5c098033e512121
Josh Isaacson <jsisaacs@iu.edu>
2019-03-21 18:40:43 -0400

    refresh and repopulate db on restart

--- src/index.js
+++ src/index.js
@@ -25,7 +25,44 @@ app.use('/messages', routes.message)
 const eraseDatabaseOnSync = true
 
 sequelize.sync({ force: eraseDatabaseOnSync }).then(() => {
+    if (eraseDatabaseOnSync) {
+        createUsersWithMessages()
+    }
+    
     app.listen(process.env.PORT, () => {
         console.log(`Listening on port ${process.env.PORT}`)
     })
 })
+
+const createUsersWithMessages = async () => {
+    await models.User.create(
+        {
+            username: 'rwieruch',
+            messages: [
+                {
+                    text: 'Published the Roat to learn React'
+                }
+            ]
+        },
+        {
+            include: [models.Message]
+        }
+    )
+
+    await models.User.create(
+        {
+            username: 'ddavids',
+            messages: [
+                {
+                    text: 'Happy to release ...'
+                },
+                {
+                    text: 'Published a complete ...'
+                }
+            ]
+        },
+        {
+            include: [models.Message]
+        }
+    )
+}
